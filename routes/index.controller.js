8f8a6f50a18520db46e49cf329da2aeab847c23a
Benjamin Chipinde <benjamin@kylix.online>
2022-07-20 01:54:59 +0300

    test config with jest && supertest

--- routes/index.controller.js
+++ routes/index.controller.js
@@ -50,7 +50,7 @@ async function postHomePage (req, res, next) {
         return res.status(400).json({ message: 'Unable to form ascii art' })
       }
       console.log(ascii)
-      return res.status(200).render('sign', { title: 'HOROSCOPE APP', name, sign: ascii, description: data.description, current_date: data.current_date, lucky_number: toAscii(zodiac) })
+      return res.status(200).render('sign', { title: name, name, sign: ascii, description: data.description, current_date: data.current_date, lucky_number: toAscii(zodiac) })
     })
   } catch (error) {
     console.error(error)
